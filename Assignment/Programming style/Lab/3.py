5314ae516aea4af2f46694cd79aa39be1d9b2030
ghoghari08 <hardikghari01@gmail.com>
2025-06-23 22:41:39 +0530

    Forth

---
+++ Assignment/Programming style/Lab/3.py
@@ -0,0 +1,11 @@
+# Creating an integer variable
+age = 30
+
+# Creating a string variable
+name = "Hardik"
+
+# Creating a float variable
+price = 19.99
+
+# Creating a boolean variable
+is_active = True
---
+++ Assignment/Programming style/Lab/4.py
@@ -0,0 +1,3 @@
+a = input("Enter You Name:")
+
+print(a)
